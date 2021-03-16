5092bca8ea69b12ae31b0d24a1f68ea92f756e8a
AswinManohar <aswinbio@gmail.com>
2021-03-16 13:53:13 +0100

    Update energy_distribution_new.py

--- energy_distribution_new.py
+++ energy_distribution_new.py
@@ -9,20 +9,9 @@ mean_int_on= 0.015384
 mean_int_off=0.000357*5
 
 
-
-
 import glob
 lists=glob.glob("/u/aswin/sp_test_reatough/individuaal_archives/new/*.ar")
 
-
-#df=pd.DataFrame()
-
-
-#df1=pd.DataFrame()
-print "ttt"
-#def integrated():
-   # df1=pd.DataFrame()
-    #df=pd.DataFrame()
 for i in lists:
  #3trim value
     command=['pdv','-jD','-t',str(i)]
@@ -37,17 +26,17 @@ for i in lists:
     #subprocess.check_output(command,shell=True)   #os.commands
        x = pd.read_csv(B,delim_whitespace=True)
        x.columns = ['a', 'b', 'bin', 'energy', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n']
-        #on_pulse = x[x.energy> mean_int_off]
-        #off_pulse = x[x.energy<0.001175]
+       on_pulse = x[x.energy> mean_int_off]
+       off_pulse = x[x.energy<0.001175]
        OFF_MEAN_SUB=x["energy"]-0.004424
         
        on_pulse_total=OFF_MEAN_SUB.iloc[375:420].sum()
        print on_pulse_total
-        #total_on_pulse=on_pulse.sum(axis = 1, skipna = True)
-       #on_pulse_mean=on_pulse['energy'].mean()
-        #mean_on_pulse = on_pulse[['energy']].mean(axis=0)
-       #mean=x['energy'].mean()
-        #mean_off_pulse = off_pulse[['energy']].mean(axis=0)
+       total_on_pulse=on_pulse.sum(axis = 1, skipna = True)
+       on_pulse_mean=on_pulse['energy'].mean()
+       mean_on_pulse = on_pulse[['energy']].mean(axis=0)
+       mean=x['energy'].mean()
+      mean_off_pulse = off_pulse[['energy']].mean(axis=0)
     
        #on_pulse_val = on_pulse/on_pulse_mean
     
@@ -61,14 +50,10 @@ for i in lists:
        # df1=df1.append(data1)
    
      
-        #return on_pulse_val
+       return on_pulse_val
 
-
-       
-#a=integrated()    
-#print a
-#np.savetxt(r'/u/aswin/sp_test_reatough/individuaal_archives/off_pulse.txt', df.values, fmt='%d')   
-#np.savetxt(r'/u/aswin/sp_test_reatough/individuaal_archives/on_pulse.txt', df1.values, fmt='%d')       
+np.savetxt(r'/u/aswin/sp_test_reatough/individuaal_archives/off_pulse.txt', df.values, fmt='%d')   
+np.savetxt(r'/u/aswin/sp_test_reatough/individuaal_archives/on_pulse.txt', df1.values, fmt='%d')       
 
 
 
